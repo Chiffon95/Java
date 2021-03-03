d5c157654a137c21537e9ee29802250589db16fb
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-03-03 17:26:06 +0900

    java ch02 variable ~ing

--- ch02/src/ch02/Print.java
+++ ch02/src/ch02/Print.java
@@ -1,6 +1,6 @@
 package ch02;
 
-public class print {
+public class Print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ ch02/src/ch02/Variable1.java
@@ -0,0 +1,27 @@
+package ch02;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int word = 'A';
+		int a = 10;
+		long l = 10000000000L;
+		long l2 = 10_000_000_000L;
+		float f = 10.0f;
+		float f2 = 10f;
+		double d = 10.0;
+		double d2 = 10.;
+		double d3 = 1e3;
+		
+		System.out.println(word);
+		System.out.println(a);
+		System.out.println(l);
+		System.out.println(l2);
+		System.out.println(f);
+		System.out.println(f2);
+		System.out.println(d);
+		System.out.println(d2);
+		System.out.println(d3);
+	}
+}
