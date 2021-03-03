9f21be7bc6b317c9704bfd799a7e8a4e898ea38b
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-03-03 16:22:18 +0900

    1

--- ch02/src/ch02/print.java
+++ ch02/src/ch02/print.java
@@ -1,8 +1,12 @@
+package ch02;
 
-public class Hello {
+public class print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
+		System.out.print("3+5=");
+		System.out.println(3+5);
 	}
+
 }
