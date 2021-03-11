87295296200694c318cde282de590b4abbfe13d4
Hyeonhak Choi (최현학) <66194623+chiffona@users.noreply.github.com>
2021-03-11 18:52:34 +0900

    2021.03.11 end -> abstract ~ing

--- ch07_OOPII/src/ch07_OOPII/OOPII_abstract.java
+++ ch07_OOPII/src/ch07_OOPII/OOPII_abstract.java
@@ -1,35 +1,60 @@
 package ch07_OOPII;
 
 abstract class Unit{
-	int x, y;
-	abstract void move (int x, int y);
+	double xPoint, yPoint;
+	double xSpeed, ySpeed;
+	abstract void move (double xPoint, double yPoint);
+	abstract void speed (double x, double y);
 	void stop() {}
 	void printfInfo() {}
 }
 
 class Marine extends Unit{
-	void move(int x, int y) {
-		this.x = x; this.y = y;
+	void move(double xPoint, double yPoint) {
+		super.xPoint = Math.round(xPoint * 100) / 100.0;
+		super.yPoint = Math.round(yPoint * 100) / 100.0;
+	}
+	void speed (double xPoint, double yPoint) {
+		super.xSpeed = Math.abs(super.xPoint - xPoint);
+		super.ySpeed = Math.abs(super.yPoint - yPoint);
+	}
+
+	void printfInfo() {
+		System.out.println(this.toString() + " :  -> "
+				+ "Move [x = " + super.xPoint + ", y = " + super.yPoint + "]");
 	}
 	public String toString() { return "Marine"; }
 }
 
 class Tank extends Unit{
-	void move(int x, int y) {
-		this.x = (int) (1.2 * x);
-		this.y = (int) (1.2 * x);
+	void move(double x, double y) {
+		super.xPoint = Math.round(xPoint * 100) / 100.0;
+		super.yPoint = Math.round(yPoint * 100) / 100.0;
+	}
+	void speed (double x, double y) {
+		super.xSpeed = Math.abs(super.xPoint - xPoint) * 1.2;
+		super.ySpeed = Math.abs(super.yPoint - yPoint) * 1.2;
 	}
 	
 	void changeMode() {
 		System.out.println("Tank mode changing");
 	}
+
+	void printfInfo() {
+		System.out.println(this.toString() + " :  -> "
+				+ "Move [x = " + super.xPoint + ", y = " + super.yPoint + "]");
+	}
 	public String toString() { return "Tank"; }
 }
 
 class Dropship extends Unit {
-	void move(int x, int y) {
-		this.x = (int) (1.5 * x);
-		this.y = (int) (1.5 * y);
+	void move(double x, double y) {
+		super.xPoint = Math.round(xPoint * 100) / 100.0;
+		super.yPoint = Math.round(yPoint * 100) / 100.0;
+	}
+	void speed (double x, double y) {
+		super.xSpeed = Math.abs(super.xPoint - xPoint) * 1.5;
+		super.ySpeed = Math.abs(super.yPoint - yPoint) * 1.5;
 	}
 	
 	void load() {
@@ -39,8 +64,13 @@ class Dropship extends Unit {
 	void unload() {
 		System.out.println("Unload!!!");
 		}
-	public String toString() { return "Dropship"; }
 	
+	void printfInfo() {
+		System.out.println(this.toString() + " :  -> "
+				+ "Move [x = " + super.xPoint + ", y = " + super.yPoint + "]");
+	}
+	public String toString() { return "Dropship"; }
+}	
 
 public class OOPII_abstract {
 
@@ -50,10 +80,21 @@ public class OOPII_abstract {
 		group[0] = new Marine();
 		group[1] = new Tank();
 		group[2] = new Dropship();
+		double xxx = 4.65, yyy = 4.65;
 		
-		for(int i = 0; i < group.length; i++) {
-			group[i].move(100, 200);
+		for (int i = 0; i < group.length; i++) {
+			
+			group[i].move(xxx, yyy);
+			group[i].printfInfo();
+			xxx += 10;
+			yyy += 10;
+			group[i].move(xxx, yyy);
+			group[i].printfInfo();
+			xxx -= 10;
+			yyy -= 10;
+			group[i].move(xxx, yyy);
+			group[i].printfInfo();
 		}
-	}
 
+	}
 }
